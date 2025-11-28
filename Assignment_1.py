24a6e2fa5c57ed8eaa01394f7088a7364e90f56e
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-11-28 21:35:23 +0530

    Fix formatting issues in Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,22 +1,22 @@
 #String formating 
-"""
+
 name=input("Enter your name:")
 age=int(input("Enter your age:"))
 
 print(f"hello {name}, you are {age} years old!")
 print("hello", name, ",", "your are", age, "years old!")
-print("hello {}, you are {} years old!".format(name, age)) """
+print("hello {}, you are {} years old!".format(name, age)) 
 
 #second question
-"""
+
 num1=int(input("Enter first number:"))
 num2=int(input("Enter second number:"))
 
 print(f"sum of {num1} and {num2} is {num1+num2}")
-print(f"substraction of {num1} and {num2} is {num1-num2}")"""
+print(f"substraction of {num1} and {num2} is {num1-num2}")
 
 #third question
-"""
+
 a=int(input("Enter 1st value:"))
 b=int(input("Enter 2st value:"))
 c=float(input("Enter 3st value:"))
@@ -25,11 +25,11 @@ a1=float(a)
 b1=float(b)
 
 avg=(a1+b1+c)/3
-print(f"Average of these values is: {avg}")"""
+print(f"Average of these values is: {avg}")
 
 
 #fourth question
-"""
+
 a=input("Enter a number:")
 int_a=int(a)
 print(f"type of {int_a} is {type(int_a)}")
@@ -37,15 +37,15 @@ float_a=float(a)
 print(f"type of {float_a} is {type(float_a)}")
 string_a=str(a)
 print(f"type of {string_a} is {type(string_a)}")
-"""
+
 
 #fifth question
-"""
+
 a=10+3*2**2
-print(a)"""
+print(a)
 
 #sixth question
-"""
+
 val1=int(input("Enter value 1:"))
 val2=int(input("Enter value 2:"))
 
@@ -53,30 +53,30 @@ temp=val1
 val1=val2
 val2=temp
 print(f"now value 1 is {val1} and value 2 is {val2}")
-"""
+
 #7th question
-"""
+
 r=float(input("Enter radius value:"))
 pi=3.14
 area=pi*r**2
-print(f"Area pf circle is: {area}")"""
+print(f"Area pf circle is: {area}")
 
 #8th question
-"""
+
 celsius = float(input("Enter temperature in celsius:"))
 ferenheit=(celsius*5/9)+32
-print(f"Temperate in ferenheit is : {ferenheit}")"""
+print(f"Temperate in ferenheit is : {ferenheit}")
 
 #ninth question
-"""
+
 p=float(input("Enter principle amount:"))
 r=float(input("Enter rate of interest:"))
 t=float(input("Enter time in years:"))
 si=(p*r*t)/100
-print(f"Simple interest of {p} amount at rate {r} for {t} years is: {si}")"""
+print(f"Simple interest of {p} amount at rate {r} for {t} years is: {si}")
 
 #tenth question
-"""
 num=float(input("Enter a decimal number:"))
 fractional_part=num-int(num)
-print(f"Integer part - {int(num)} \nFractional part - {round(fractional_part, 2)}")"""
+print(f"Integer part - {int(num)} \nFractional part - {round(fractional_part, 2)}")
+
