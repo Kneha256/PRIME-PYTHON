177938b517ff558fc5b7d24b64afb08c33d3458c
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-11-28 14:36:44 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,45 @@
+print("Hello World1", end=" ")
+print("Hello World2")
+
+#variable - location inside memory to store some value
+#variable/identifiers should be meaningful and start with _, letter(both lowercase and uppercase)
+_name="neha"
+Pi=3.14
+age2=22
+
+print("my name is", _name,"and age is",age2)
+# both indentation and cse senstivity is importand in python
+
+#datatypes in python
+#integer
+a=10
+print(type(a))
+
+#float
+b=3.14
+print(type(b))
+
+#string - '' or "" or ''' ''' or """ """
+c="hello"
+print(type(c))
+
+#boolean T and F should always be capitalized
+d=True
+print(type(d))
+
+#None - while we done want to give value but want to create a variabl
+e=None
+print(type(e))
+
+#keywords - reserved words in python which have some special meaning and cannot be used as variable names
+#comments - single line(#) and multi line (''' ''' or """ """)
+
+#style guide - use for creating variable
+#snake case - my_name_is_neha
+#camel case - myNameIsNeha
+#pascal case - MyNameIsNeha
+
+a = 5
+b = 10
+sum=a + b
+print("sum of two numbers is", sum)
