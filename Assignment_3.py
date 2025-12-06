a1620d56545a4bbec4d8e54ddb62fd6e97134918
Kumari Neha <158587756+Kneha256@users.noreply.github.com>
2025-12-06 18:33:06 +0530

    Add files via upload

--- Assignment_3.py
+++ Assignment_3.py
@@ -1,5 +1,15 @@
+#str-palindrome
+str=input("Enter a string: ")
+
+str_rev=str[::-1]
+
+if str == str_rev:
+    print("It is palindrome")
+else:
+    print("Not a palindrome")  
+
+
 #find avg of el in list
-"""
 list=[23,45,67,89,54,78.9]
 
 sum=0
@@ -9,6 +19,7 @@ for i in list:
 
 avg=sum/n
 print(f"Average of elements in list is: {avg}")
+
 
 #take 2 list as input merge them and sort
 def merg_sort(L1,L2):
@@ -26,6 +37,8 @@ L2=list(map(int,input("Enter nums:").split()))
 Sorted_List=merg_sort(L1,L2)
 print(Sorted_List)
 
+
+
 #odd tuple and even tuple
 t=(1,2,3,4,5,6,7,8,9,10)
 t_list=list(t)
@@ -41,19 +54,9 @@ for el in t_list:
 print(tuple(Even))
 print(tuple(Odd)) 
 
-#dictionary question:
-student = {
-    "neha":45,
-    "shreya":42,
-    "kavya":38,
-    "rahul":48,
-    "reema":32,
-    "shruti":44
-}
 
-print(type(student))
-print(student) """
 
+#return duplicates
 list=list(map(int,input("Enter numbers : ").split()))
 
 seen=set()
@@ -65,6 +68,71 @@ for el in list:
     else:
         seen.add(el)
 
-print(duplicate)
+print(duplicate) 
 
 
+#dictionary question - student management
+student = {}
+def std(action_key):
+    match(action_key):
+        case "A":
+            stu=input("Enter Student name: ")
+            marks=int(input("Enter marks : "))
+            student[stu]=marks
+            print(student)
+        case "B":
+            stu=input("enter student name: ")
+            if stu in student.keys():
+                marks=int(input("Enter marks: "))
+                student.update({stu:marks})
+                print(f"Updated marks:{student}")
+            else:
+                marks=int(input("Student not found! Enter marks: "))
+                student.update({stu:marks})
+                print(f"Updated marks:{student}")
+        case "C":
+            stu=input("Enter student name : ")
+            if stu in student.keys():
+                print(f"{stu}'s data exist")
+            else:
+                print(f"No data found related to {stu}")
+        case "D":
+            for name, marks in student.items():
+                print(f"name : {name}, marks : {marks}")
+        case _: print ("default input")
+
+print("Enter A for adding new student details.\nEnter B for updating marks of a student")
+print("Enter C to seach a student.\nEnter D for printing all students data along with marks")
+print("Type stop to exit")
+while(True):
+    action_key=input("Enter you Action_Key: ").upper()
+    if(action_key == "STOP"):
+        break
+    std(action_key)
+
+
+
+#common element in two list
+def check_common(L1, L2):
+    for el in L1:
+        if el in L2:
+            print(f"Share common element")
+            break
+print("share no common elements")
+
+
+L1=list(map(int, input("Enter list items with space: ").split()))
+L2=list(map(int, input("Enter list items with space: ").split()))
+
+check_common(L1,L2)
+
+
+
+#dictionary maping question 
+
+words =["apple","banana","kiwi","cherry","mango"]
+dic={}
+for el in words:
+    dic[el]=(len(el))
+
+print(dic)
